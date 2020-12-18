93a583b9b000bc728407cfcbd7c5ca57f1410699
Sara Green <smg105c@gmail.com>
2020-12-17 18:44:31 -0800

    preparing for deployment

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIEDEX = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'dev'
+app.use(morgan(morganSetting))
 app.use(helmet())
 app.use(cors())
 
@@ -46,8 +47,18 @@ app.get('/movies', function handleGetMovies(req, res) {
 
 })
 
-const PORT = 8000;
+app.use((error, req, res, next) => {
+    let response
+    if(process.env.NODE_ENV === 'production') {
+        response = { error: { message: 'server error' }}
+    } else {
+        response = { error }
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-    console.log('Server is listening on PORT 8000')
+    console.log(`Server is listening on PORT ${PORT}`)
 })
